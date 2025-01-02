1d1c8cc5fa1caebdd40bf2184fc786d11bafa8bb
0w0miki <miki0w0@hotmail.com>
2025-01-02 10:01:25 +0800

    fix: prevent `this` issues by using arrow functions

--- src/toolbar/container.jsx
+++ src/toolbar/container.jsx
@@ -2,48 +2,50 @@ import { debounce, throttle } from "rambdax"
 import { isEditingText, isTextSelected } from "../core"
 
 export default class toolbarContainer {
+  container
+
   constructor(element) {
     this.container = element
   }
 
-  registerContainerEvents() {
-    parent.document.addEventListener("focusout", this.#onBlur)
-    this.container.addEventListener("transitionend", this.#onToolbarTransitionEnd)
+  registerContainerEvents = () => {
+    parent.document.addEventListener("focusout", this.onBlur)
+    this.container.addEventListener("transitionend", this.onToolbarTransitionEnd)
 
     const mainContentContainer = parent.document.getElementById(
       "main-content-container",
     )
-    mainContentContainer.addEventListener("scroll", this.#onScroll, {
+    mainContentContainer.addEventListener("scroll", this.onScroll, {
       passive: true,
     })
   }
 
-  unregisterContainerEvents() {
+  unregisterContainerEvents = () => {
     const mainContentContainer = parent.document.getElementById(
       "main-content-container",
     )
-    mainContentContainer.removeEventListener("scroll", this.#onScroll, {
+    mainContentContainer.removeEventListener("scroll", this.onScroll, {
       passive: true,
     })
-    this.container.removeEventListener("transitionend", this.#onToolbarTransitionEnd)
-    parent.document.removeEventListener("focusout", this.#onBlur)
+    this.container.removeEventListener("transitionend", this.onToolbarTransitionEnd)
+    parent.document.removeEventListener("focusout", this.onBlur)
   }
 
-  #onToolbarTransitionEnd(e) {
+  onToolbarTransitionEnd = () => {
     if (this.container.style.opacity === "0") {
       this.container.style.top = "0"
       this.container.style.left = "-99999px"
     }
   }
 
-  #onBlur(e) {
+  onBlur = (e) => {
     // Update toolbar visibility upon activeElement change.
     if (!isEditingText()) {
       this.hideToolbar()
     }
   }
 
-  #onScroll(e) {
+  onScroll = (e) => {
     // There is a large gap between 2 displays of the toolbar, so a large
     // ms number is acceptable.
     throttle(this.hideToolbar, 1000)()
@@ -54,13 +56,13 @@ export default class toolbarContainer {
     }, 100)()
   }
 
-  hideToolbar() {
+  hideToolbar = () => {
     if (this.container.style.opacity !== "0") {
       this.container.style.opacity = "0"
     }
   }
 
-  async showToolbar() {
+  showToolbar = async () => {
     const curPos = await logseq.Editor.getEditingCursorPosition()
     if (curPos != null) {
       this.container.style.top = `${curPos.top + curPos.rect.y - 35}px`
